7b34c7b6fb0df379b80c81690b343e5a1adaa372
Baiysh Mamatov <altynbekovich.98@gmail.com>
2024-07-20 21:43:47 +0600

    save git

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+        String git;
+        Scanner scan= new Scanner(System.in);
+        git = scan.next();
+        if (git=="git"){
+            System.out.println("this is git");
+        }
     }
 }
